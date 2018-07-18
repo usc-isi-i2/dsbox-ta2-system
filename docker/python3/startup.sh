cd1aaa5768fa9213980c8eb6ca6a7f8d4e116dc3
RqS <shaorunqi@gmail.com>
2018-07-18 16:24:54 -0700

    docker files

--- docker/python3/startup.sh
+++ docker/python3/startup.sh
@@ -1,4 +1,4 @@
-
+#! /bin/bash
 #execute ta1-pipeline code to generate submission product
 cd /user_opt/dsbox/dsbox-ta2/python
-python3 train_test.py $TEMPLATE $CONFIG_DIR
+python3 train_test_aws.py $CONFIG_DIR
